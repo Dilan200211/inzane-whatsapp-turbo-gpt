5f4ac12a939c188ea566d5d2a310cf3c35f25626
Dilan200211 <120083894+Dilan200211@users.noreply.github.com>
2023-05-16 19:54:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const sessionName = "harshitethic";
 const donet = "https://harshitethic.in";
-const owner = ["918708056374"]; // Put your number here ex: ["91xxxxxxxxx"]
+const owner = ["94783405322"]; // Put your number here ex: ["91xxxxxxxxx"]
 const {
   default: sansekaiConnect,
   useMultiFileAuthState,
